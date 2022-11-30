c15c0bcf0b7df8c45b62802a728af7a4c8efedf4
antonio <antonio.silvajunior.as@gmail.com>
2022-11-30 06:32:51 -0300

    exercicio2

---
+++ Exercicio1.2/exercicio/src/Pessoa.java
@@ -0,0 +1,45 @@
+
+    public class Pessoa {
+
+        private String nome;
+        private int idade;
+        private String email;
+
+        public Pessoa(String nome, int idade, String email) {
+            this.nome = nome;
+            this.idade = idade;
+            this.email = email;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public int getIdade() {
+            return idade;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String VerificarIdade() {
+            if (idade >= 18) {
+                System.out.println(nome);
+                System.out.println(email);
+            }
+            return "";
+        }
+    }
--- condicionais/src/lacos/exercicio1/Exercicio1_DoWhile.java
+++
@@ -1,7 +0,0 @@
-//Some números dados pelo usuário até que ele digite um número negativo.
-
-package lacos.exercicio1;
-
-public class Exercicio1_DoWhile {
-
-}
